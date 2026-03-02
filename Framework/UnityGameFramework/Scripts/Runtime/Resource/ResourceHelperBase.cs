d9f9bd132b6f3f7c05083294ad7083261ba6d08d
Lin <li18202213260@gmail.com>
2026-03-02 18:33:04 +0800

    fix: 在 ResourceHelperBase 中添加新接口方法的抽象声明

--- Framework/UnityGameFramework/Scripts/Runtime/Resource/ResourceHelperBase.cs
+++ Framework/UnityGameFramework/Scripts/Runtime/Resource/ResourceHelperBase.cs
@@ -27,5 +27,29 @@ namespace UnityGameFramework.Runtime
         /// </summary>
         public abstract void UnloadScene(string sceneAssetName,
             GameFramework.Resource.UnloadSceneCallbacks callbacks, object userData);
+
+        /// <summary>
+        /// 加载资源
+        /// </summary>
+        public abstract void LoadAsset(string assetName, System.Type assetType,
+            GameFramework.Resource.LoadAssetCallbacks callbacks, object userData);
+
+        /// <summary>
+        /// 加载场景
+        /// </summary>
+        public abstract void LoadScene(string sceneAssetName,
+            GameFramework.Resource.LoadSceneCallbacks callbacks, object userData);
+
+        /// <summary>
+        /// 加载二进制/原始文件
+        /// </summary>
+        public abstract void LoadBinary(string binaryAssetName,
+            GameFramework.Resource.LoadBinaryCallbacks callbacks, object userData);
+
+        /// <summary>
+        /// 实例化资源
+        /// </summary>
+        public abstract void InstantiateAsset(string assetName,
+            GameFramework.Resource.LoadAssetCallbacks callbacks, object userData);
     }
 }
