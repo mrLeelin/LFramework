bbb937f2e510850047d346310f9c4625275ce855
Lin <li18202213260@gmail.com>
2026-06-30 18:36:02 +0800

    增加 IgnoreInterface

--- Framework/LFramework/Scripts/Runtime/Injection/IInjectable.cs
+++ Framework/LFramework/Scripts/Runtime/Injection/IInjectable.cs
@@ -3,6 +3,7 @@ namespace LFramework.Runtime
     /// <summary>
     /// Implemented by generated partial classes so runtime injection can avoid reflection.
     /// </summary>
+    [IgnoreInterface]
     public interface IInjectable
     {
         void Inject(IServiceResolver resolver);
